a1907b3d174df65f6aa69017aae765b7438e11ff
anabelaSantos <anabelamsantos@gmail.com>
2023-02-20 14:21:22 +0000

    exercise complete

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
+const express = require("express"); //importing library from the node module
 
+const app = express();
+const port = 3000; //5000 for backend RESTful APIs, 3000 for front end
+
+app.use(express.static("public")); //express.static is an express method for serving static files
+
+app.listen(port, () => {
+  console.log(`listening on hppt://localhost${port}`);
+});
